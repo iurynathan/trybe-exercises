38d4172964bbf344d7294498834d45d7436c3eaa
Iury Nathan <iurynathan@gmail.com>
2020-09-01 18:23:49 -0300

    initial commit

---
+++ introduction/bloco_04/dia_03/exercicio01.js
@@ -0,0 +1,10 @@
+let n = 5;
+let asteriscos = "*";
+let newAsteriscos = "";
+
+for (let i = 0; i < n; i += 1) {
+  newAsteriscos = newAsteriscos + asteriscos;
+}
+for (let i = 0; i < n; i += 1) {
+  console.log(newAsteriscos);
+}
---
+++ introduction/bloco_04/dia_03/exercicio02.js
@@ -0,0 +1,8 @@
+let n = 5;
+let asterisco = '*';
+let newAsterisco = '';
+
+for (i = 0; i < n; i += 1) {
+  newAsterisco = newAsterisco + asterisco
+  console.log(newAsterisco)
+}
