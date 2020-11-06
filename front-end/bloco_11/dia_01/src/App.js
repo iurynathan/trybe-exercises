37af9684f4d590cd26b745ebbc8c2b644d0d39d6
Iury Nathan <iurynathan@gmail.com>
2020-11-05 22:16:38 -0300

    feito exercicio

--- front-end/bloco_11/dia_01/src/App.js
+++ front-end/bloco_11/dia_01/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const taskList = [
+  'Tarefa 1',
+  'Tarefa 2',
+  'Tarefa 3',
+  'Tarefa 4'
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {taskList.map((tasks) => task(tasks))}
     </div>
   );
 }
